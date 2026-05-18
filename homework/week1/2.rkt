1738f7badd88db461f752b80ce16fa0df2aa0f37
ndfnd <hellpl4y@gmail.com>
2026-05-18 14:53:00 +0500

    complete 1 & 2 tasks of hw1

---
+++ homework/week1/2.rkt
@@ -0,0 +1,16 @@
+#lang planet dyoo/simply-scheme:2
+
+; Write a procedure squares that takes a sentence of numbers as its argument and
+; returns a sentence of the squares of the numbers:
+; > (squares ’(2 3 4 5))
+; (4 9 16 25)
+
+
+(define (square x) (* x x))
+  
+(define (squares numbers)
+  (if (empty? numbers)
+      '()
+      (cons (square (first numbers)) (squares (bf numbers)))))
+
+(squares '(2 3 4 5)) ; (4 9 16 25)
