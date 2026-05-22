2218a00bc3fbd2332fe670c3b6aaa182c305d082
ndfnd <hellpl4y@gmail.com>
2026-05-22 19:33:50 +0500

    complete lab3 of week2

---
+++ labs/week2/3.rkt
@@ -0,0 +1,6 @@
+; Consider a Scheme function g for which the expression
+; ((g) 1)
+; returns the value 3 when evaluated. Determine how many arguments g has. In one word, also describe as
+; best you can the type of value returned by g.
+
+; g has no arguments. It returns a procedure that takes one numeric argument and returns numeric result
