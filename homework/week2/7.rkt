93f6a5cf45ea17167109319f363be63ad1e720b4
ndfnd <hellpl4y@gmail.com>
2026-05-25 11:55:01 +0500

    complete hw7 of week2

---
+++ homework/week2/7.rkt
@@ -0,0 +1,28 @@
+#lang planet dyoo/simply-scheme:2
+
+; SICP Exercise 1.46:
+
+(define (iterative-improve good-enough? improve)
+  (lambda (guess)
+    (if (good-enough? guess)
+        guess
+        ((iterative-improve good-enough? improve) (improve guess))
+        )
+    ))
+
+(define (sqrt x)
+  (define (square x) (* x x))
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (average x y)
+    (/ (+ x y) 2))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  ((iterative-improve
+     (lambda (guess) (good-enough? guess x))
+     (lambda (guess) (improve guess x)))
+   1.0))
+
+(sqrt 9) ; 3.0
+(sqrt 15) ; 3.87
+(sqrt 36) ; 6.0
