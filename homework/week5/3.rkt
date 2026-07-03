c06f178542d27a39ad1e1b5d8c9858061270b1e3
ndfnd <hellpl4y@gmail.com>
2026-07-03 17:59:43 +0500

    complete hw3 of week5

---
+++ homework/week5/3.rkt
@@ -0,0 +1,11 @@
+#lang sicp
+
+; SICP Exercise 2.32:
+
+(define (subsets s)
+  (if (null? s)
+      (list '())
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x) (cons (car s) x)) rest)))))
+
+(subsets '(1 2 3)) ; (() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3))
