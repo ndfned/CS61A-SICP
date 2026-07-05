a20bf0f4cd14cd44589e2b75d0fadf27489e2f73
ndfnd <hellpl4y@gmail.com>
2026-07-05 20:11:27 +0500

    complete hw5 of week5

---
+++ homework/week5/5.rkt
@@ -0,0 +1,30 @@
+#lang sicp
+
+; SICP Exercise 2.35:
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (_x) 1) (enumerate-tree t))))
+
+
+(define x (cons (list 1 2) (list 3 4)))
+(count-leaves x) ; 4
+(count-leaves (list x x)) ; 8
