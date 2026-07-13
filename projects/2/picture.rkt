f2c375248f2f4508a9c4842129c9d8f357c6ac0d
ndfnd <hellpl4y@gmail.com>
2026-07-13 13:47:21 +0500

    wip project2

--- projects/2/picture.rkt
+++ projects/2/picture.rkt
@@ -131,3 +131,9 @@
 
 ; ===============================================
 ; my code starts here
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+	(below painter (beside smaller smaller)))))
