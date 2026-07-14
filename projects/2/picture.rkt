42f4d525b0f0fa6da769bc56a4cc672a61cf1683
ndfnd <hellpl4y@gmail.com>
2026-07-14 17:12:42 +0500

    run format

--- projects/2/picture.rkt
+++ projects/2/picture.rkt
@@ -8,18 +8,18 @@
   (if (= n 0)
       painter
       (let ((smaller (right-split painter (- n 1))))
-	(beside painter (below smaller smaller)))))
+        (beside painter (below smaller smaller)))))
 
 (define (corner-split painter n)
   (if (= n 0)
       painter
       (let ((up (up-split painter (- n 1)))
-	    (right (right-split painter (- n 1))))
-	(let ((top-left (beside up up))
-	      (bottom-right (below right right))
-	      (corner (corner-split painter (- n 1))))
-	  (beside (below painter top-left)
-		  (below bottom-right corner))))))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
 
 (define (square-limit painter n)
   (let ((quarter (corner-split painter n)))
@@ -29,14 +29,14 @@
 (define (square-of-four tl tr bl br)
   (lambda (painter)
     (let ((top (beside (tl painter) (tr painter)))
-	  (bottom (beside (bl painter) (br painter))))
+          (bottom (beside (bl painter) (br painter))))
       (below bottom top))))
 
 (define (identity x) x)
 
 (define (flipped-pairs painter)
   (let ((combine4 (square-of-four identity flip-vert
-				  identity flip-vert)))
+                                  identity flip-vert)))
     (combine4 painter)))
 
 ;; or
@@ -46,87 +46,87 @@
 
 (define (square-limit painter n)
   (let ((combine4 (square-of-four flip-horiz identity
-				  rotate180 flip-vert)))
+                                  rotate180 flip-vert)))
     (combine4 (corner-split painter n))))
 
 (define (frame-coord-map frame)
   (lambda (v)
     (add-vect
-     (origin-frame frame)
-     (add-vect (scale-vect (xcor-vect v)
-			   (edge1-frame frame))
-	       (scale-vect (ycor-vect v)
-			   (edge2-frame frame))))))
+      (origin-frame frame)
+      (add-vect (scale-vect (xcor-vect v)
+                            (edge1-frame frame))
+                (scale-vect (ycor-vect v)
+                            (edge2-frame frame))))))
 
 (define (segments->painter segment-list)
   (lambda (frame)
     (for-each
-     (lambda (segment)
-       (draw-line
-	((frame-coord-map frame) (start-segment segment))
-	((frame-coord-map frame) (end-segment segment))))
-     segment-list)))
+      (lambda (segment)
+        (draw-line
+          ((frame-coord-map frame) (start-segment segment))
+          ((frame-coord-map frame) (end-segment segment))))
+      segment-list)))
 
 (define (draw-line v1 v2)
   (penup)
   (setxy (- (* (xcor-vect v1) 200) 100)
-	 (- (* (ycor-vect v1) 200) 100))
+         (- (* (ycor-vect v1) 200) 100))
   (pendown)
   (setxy (- (* (xcor-vect v2) 200) 100)
-	 (- (* (ycor-vect v2) 200) 100)))
+         (- (* (ycor-vect v2) 200) 100)))
 
 (define (transform-painter painter origin corner1 corner2)
   (lambda (frame)
     (let ((m (frame-coord-map frame)))
       (let ((new-origin (m origin)))
-	(painter
-	 (make-frame new-origin
-		     (sub-vect (m corner1) new-origin)
-		     (sub-vect (m corner2) new-origin)))))))
+        (painter
+          (make-frame new-origin
+                      (sub-vect (m corner1) new-origin)
+                      (sub-vect (m corner2) new-origin)))))))
 
 (define (flip-vert painter)
   (transform-painter painter
-		     (make-vect 0.0 1.0)
-		     (make-vect 1.0 1.0)
-		     (make-vect 0.0 0.0)))
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
 
 (define (shrink-to-upper-right painter)
   (transform-painter painter
-		    (make-vect 0.5 0.5)
-		    (make-vect 1.0 0.5)
-		    (make-vect 0.5 1.0)))
+                     (make-vect 0.5 0.5)
+                     (make-vect 1.0 0.5)
+                     (make-vect 0.5 1.0)))
 
 (define (rotate90 painter)
   (transform-painter painter
-		     (make-vect 1.0 0.0)
-		     (make-vect 1.0 1.0)
-		     (make-vect 0.0 0.0)))
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
 
 (define (squash-inwards painter)
   (transform-painter painter
-		     (make-vect 0.0 0.0)
-		     (make-vect 0.65 0.35)
-		     (make-vect 0.35 0.65)))
+                     (make-vect 0.0 0.0)
+                     (make-vect 0.65 0.35)
+                     (make-vect 0.35 0.65)))
 
 (define (beside painter1 painter2)
   (let ((split-point (make-vect 0.5 0.0)))
     (let ((paint-left
-	   (transform-painter painter1
-			      (make-vect 0.0 0.0)
-			      split-point
-			      (make-vect 0.0 1.0)))
-	  (paint-right
-	   (transform-painter painter2
-			      split-point
-			      (make-vect 1.0 0.0)
-			      (make-vect 0.5 1.0))))
+            (transform-painter painter1
+                               (make-vect 0.0 0.0)
+                               split-point
+                               (make-vect 0.0 1.0)))
+          (paint-right
+            (transform-painter painter2
+                               split-point
+                               (make-vect 1.0 0.0)
+                               (make-vect 0.5 1.0))))
       (lambda (frame)
-	(paint-left frame)
-	(paint-right frame)))))
+        (paint-left frame)
+        (paint-right frame)))))
 
 (define full-frame (make-frame (make-vect -0.5 -0.5)
-			       (make-vect 2 0)
-			       (make-vect 0 2)))
+                               (make-vect 2 0)
+                               (make-vect 0 2)))
 
 
 ; ===============================================
@@ -136,4 +136,4 @@
   (if (= n 0)
       painter
       (let ((smaller (up-split painter (- n 1))))
-	(below painter (beside smaller smaller)))))
+        (below painter (beside smaller smaller)))))
