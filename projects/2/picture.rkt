aaa7773f698b4e3156511d63a7c97909bdd52528
ndfnd <hellpl4y@gmail.com>
2026-07-15 15:51:07 +0500

    wip project2

--- projects/2/picture.rkt
+++ projects/2/picture.rkt
@@ -164,3 +164,9 @@
   (make-vect
     (* s (xcor-vect v))
     (* s (ycor-vect v))))
+
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+(define (origin-frame frame) (car frame))
+(define (edge1-frame frame) (cadr frame))
+(define (edge2-frame frame) (caddr frame))
