891a5153eaa6740dfb132119589d218d8af67526
ndfnd <hellpl4y@gmail.com>
2026-07-15 15:44:52 +0500

    wip project2

--- projects/2/picture.rkt
+++ projects/2/picture.rkt
@@ -147,3 +147,20 @@
 
 (define right-split (split beside below))
 (define up-split (split below beside))
+
+(define (make-vect x y) (cons x y))
+(define (xcor-vect v) car)
+(define (ycor-vect v) cdr)
+
+(define (add-vect v1 v2)
+  (make-vect
+    (+ (xcor-vect v1) (xcor-vect v2))
+    (+ (ycor-vect v1) (ycor-vect v2))))
+(define (sub-vect v1 v2)
+  (make-vect
+    (- (xcor-vect v1) (xcor-vect v2))
+    (- (ycor-vect v1) (ycor-vect v2))))
+(define (scale-vect s v)
+  (make-vect
+    (* s (xcor-vect v))
+    (* s (ycor-vect v))))
