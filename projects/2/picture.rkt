debb54667ef6f500c902eb7b1054c3032a3ec1aa
ndfnd <hellpl4y@gmail.com>
2026-07-20 13:35:27 +0500

    wip project2

--- projects/2/picture.rkt
+++ projects/2/picture.rkt
@@ -207,3 +207,19 @@
                      (make-vect 0 1)
                      (make-vect 1 1)
                      (make-vect 0 0)))
+
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0 0.5)))
+    (let ((paint-bottom
+            (transform-painter painter1
+                               (make-vect 0 0)
+                               (make-vect 1 0)
+                               split-point))
+          (paint-top
+            (transform-painter painter2
+                               split-point
+                               (make-vect 1 0.5)
+                               (make-vect 0 1))))
+      (lambda (frame)
+        (paint-bottom frame)
+        (paint-top frame)))))
