f568a6eee20faac02145fff89399448a540c4bb3
ndfnd <hellpl4y@gmail.com>
2026-07-20 13:04:00 +0500

    wip project2

--- projects/2/picture.rkt
+++ projects/2/picture.rkt
@@ -191,3 +191,9 @@
                        (make-segment (make-vect 0.5 1) (make-vect 1 0.5))
                        (make-segment (make-vect 1 0.5) (make-vect 0.5 0))
                        (make-segment (make-vect 0.5 0) (make-vect 0 0.5)))))
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1 0)
+                     (make-vect 0 0)
+                     (make-vect 1 1)))
