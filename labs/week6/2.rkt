eaef53d6034636c8c0a0c66593522c099285980b
ndfnd <hellpl4y@gmail.com>
2026-07-21 12:08:34 +0500

    wip lab2 of week6

--- labs/week6/2.rkt
+++ labs/week6/2.rkt
@@ -26,3 +26,20 @@
 (union-set
   (list 1 2 3)
   (list 1 3 4 5)) ; (2 1 3 4 5)
+
+; Exercise 2.60
+(define (element-of-set2? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+(element-of-set2? 2 (list 2 3 2 1 3 2 2)) ; #t
+
+(define adjoin-set2 cons)
+(adjoin-set2 2 (list 2 3 2 1 3 2 2)) ; (2 2 3 2 1 3 2 2)
+
+(define (union-set2 set1 set2)
+  (cond ((or (null? set1) (null? set2)) set2)
+        (else (cons (car set1) (union-set2 (cdr set1) set2)))))
+(union-set2
+  (list 1 2 3)
+  (list 1 3 4 5)) ; (1 2 3 1 3 4 5)
